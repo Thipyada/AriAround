831d50caa00d9a8243151c729719eff739829546
GigiTYD <thipyada.zheng@gmail.com>
2023-09-27 18:07:45 -0400

    first commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express'
+import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const PORT = 8080
+
+const app: Application = express()
+
+app.disable('x-powered-by')
+app.use(morgan('dev'))
+app.use(urlencoded({ extended: true }))
+app.use(json())
+app.use(cors())
+
+// http://localhost:8080/test
+
+//middleware
+
+//Routes
+
+// run yarn start
+app.get('/test', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Core API is running on ${PORT}`)
+})
