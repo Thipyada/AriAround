53bf49113c6b0e1e6ae4256435d4defec9c2fc86
GigiTYD <thipyada.zheng@gmail.com>
2023-09-27 22:28:08 -0400

    permission route initiate

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -5,22 +5,16 @@ import { Router } from 'express'
 const router = Router()
 const prisma = new PrismaClient()
 
-// GET /api/permissions
-// router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const permissions = await prisma.permission.findMany()
-//     res.status(200).json({ data: permissions })
-//   } catch (error) {
-//     res.status(400).json({ message: 'error', error })
-//   }
-// })
+// Query
 
-// GET /api/permissions/:id
+// GET all
 
-// POST /api/permissions
+// GET by id
 
-// PUT /api/permissions/:id
+// POST
 
-// DELETE /api/permissions/:id
+// PUT
+
+// DELETE
 
 export default router
