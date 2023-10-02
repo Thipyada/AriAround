249f61ed838112c141a68f6e50331cd0c6e541a5
GigiTYD <thipyada.zheng@gmail.com>
2023-10-01 21:58:03 -0400

    router update

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,18 +1,72 @@
 import { PrismaClient } from '@prisma/client'
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { Router } from 'express'
+import { z } from 'zod'
 
 const router = Router()
 const prisma = new PrismaClient()
 
-// GET all
+const adminSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  profilePic: z.string().optional(),
+  phone: z.string().optional(),
+  permissions: z.array(
+    z.object({
+      feature: z.string(),
+      show: z.boolean(),
+      permissionFeature: z.object({
+        feature: z.string(),
+        show: z.boolean()
+      })
+    })
+  )
+})
+
+function ValidateAdmin(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (Array.isArray(req.body)) {
+      req.body.forEach((admin) => {
+        adminSchema.parse(admin)
+      })
+    } else {
+      adminSchema.parse(req.body)
+    }
+    next()
+  } catch (error) {
+    res.status(400).json({ message: 'error', error })
+    return
+  }
+}
+
+// GET all + query
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const admins = await prisma.admin.findMany({
-      include: {
-        permissions: true
+    const { name, email } = req.query
+    const where: Record<string, unknown> = {}
+
+    if (name) {
+      const adminName = {
+        contains: name as string,
+        mode: 'insensitive'
       }
+      where.name = adminName
+    }
+    if (email) {
+      const adminEmail = {
+        contains: email as string,
+        mode: 'insensitive'
+      }
+      where.email = adminEmail
+    }
+    const admins = await prisma.admin.findMany({
+      where: where
     })
+
+    if (!admins) {
+      res.status(404).json({ message: 'Not Found' })
+      return
+    }
     res.status(200).json({ data: admins })
   } catch (error) {
     res.status(400).json({ message: 'error', error })
@@ -38,29 +92,28 @@ router.get('/:id', async (req: Request, res: Response) => {
 })
 
 // POST
-router.post('/', async (req: Request, res: Response) => {
-  console.log('body', req.body)
+router.post('/', ValidateAdmin, async (req: Request, res: Response) => {
   try {
-    const { name, email, profilePic, phone, permissions } = req.body
-    await prisma.admin.create({
-      data: {
-        name: name,
-        email: email,
-        profilePic: profilePic,
-        phone: phone,
-        permissions: {
-          set: permissions
+    if (Array.isArray(req.body)) {
+      await prisma.admin.createMany({
+        data: req.body
+      })
+      res.status(200).json({ message: 'admins Created' })
+    } else {
+      await prisma.admin.create({
+        data: {
+          ...req.body
         }
-      }
-    })
-    res.status(201).json({ message: 'admin Created' })
+      })
+      res.status(200).json({ message: 'admin Created' })
+    }
   } catch (error) {
     res.status(400).json({ message: 'error', error })
   }
 })
 
 // PUT
-router.put('/:id', async (req: Request, res: Response) => {
+router.put('/:id', ValidateAdmin, async (req: Request, res: Response) => {
   try {
     const admin = await prisma.admin.findUnique({
       where: {
--- src/routes/organization.ts
+++ src/routes/organization.ts
@@ -1,18 +1,71 @@
 import { PrismaClient } from '@prisma/client'
 import { Request, Response, NextFunction } from 'express'
 import { Router } from 'express'
+import { z } from 'zod'
 
 const router = Router()
 const prisma = new PrismaClient()
 
-// GET all
+const organizationSchema = z.object({
+  name: z.string(),
+  Instagram: z.string().optional(),
+  Facebook: z.string().optional(),
+  type: z.string(),
+  status: z.string()
+})
+
+function ValidateOrganization(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (Array.isArray(req.body)) {
+      req.body.forEach((organization) => {
+        organizationSchema.parse(organization)
+      })
+    } else {
+      organizationSchema.parse(req.body)
+    }
+    next()
+  } catch (error) {
+    res.status(400).json({ message: 'error', error })
+    return
+  }
+}
+
+// GET all + query
 router.get('/', async (req: Request, res: Response) => {
   try {
+    const { name, type, status } = req.query
+    const where: Record<string, unknown> = {}
+    if (name) {
+      const organizationName = {
+        contains: name as string,
+        mode: 'insensitive'
+      }
+      where.name = organizationName
+    }
+    if (type) {
+      const organizationType = {
+        contains: type as string,
+        mode: 'insensitive'
+      }
+      where.type = organizationType
+    }
+    if (status) {
+      const organizationStatus = {
+        contains: status as string,
+        mode: 'insensitive'
+      }
+      where.status = organizationStatus
+    }
     const organizations = await prisma.organization.findMany({
+      where: where,
       include: {
         users: true
       }
     })
+    if (!organizations) {
+      res.status(404).json({ message: 'Not Found' })
+      return
+    }
     res.status(200).json({ data: organizations })
   } catch (error) {
     res.status(400).json({ message: 'error', error })
@@ -41,8 +94,7 @@ router.get('/:id', async (req: Request, res: Response) => {
 })
 
 // POST
-router.post('/', async (req: Request, res: Response) => {
-  console.log('body', req.body)
+router.post('/', ValidateOrganization, async (req: Request, res: Response) => {
   try {
     if (Array.isArray(req.body)) {
       await prisma.organization.createMany({
@@ -64,30 +116,34 @@ router.post('/', async (req: Request, res: Response) => {
 })
 
 // PUT
-router.put('/:id', async (req: Request, res: Response) => {
-  try {
-    const organization = await prisma.organization.findUnique({
-      where: {
-        id: req.params.id
+router.put(
+  '/:id',
+  ValidateOrganization,
+  async (req: Request, res: Response) => {
+    try {
+      const organization = await prisma.organization.findUnique({
+        where: {
+          id: req.params.id
+        }
+      })
+      if (!organization) {
+        res.status(404).json({ message: 'Not Found' })
+        return
       }
-    })
-    if (!organization) {
-      res.status(404).json({ message: 'Not Found' })
-      return
+      await prisma.organization.update({
+        where: {
+          id: req.params.id
+        },
+        data: {
+          ...req.body
+        }
+      })
+      res.status(200).json({ message: 'Organization Updated' })
+    } catch (error) {
+      res.status(400).json({ message: 'error', error })
     }
-    await prisma.organization.update({
-      where: {
-        id: req.params.id
-      },
-      data: {
-        ...req.body
-      }
-    })
-    res.status(200).json({ message: 'Organization Updated' })
-  } catch (error) {
-    res.status(400).json({ message: 'error', error })
   }
-})
+)
 
 // DELETE
 router.delete('/:id', async (req: Request, res: Response) => {
